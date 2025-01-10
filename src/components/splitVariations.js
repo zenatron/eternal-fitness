6dd19277e21988368f422af286d518b1a3e3cb77
Phil Vishnevsky <gadgetsphilop36@gmail.com>
2025-01-10 15:22:39 -0500

    split up code into components

---
+++ src/components/splitVariations.js
@@ -0,0 +1,73 @@
+export const twoDaySplits = {
+    variation1: ['Upper', 'Rest', 'Lower', 'Rest', 'Rest', 'Rest', 'Rest'],
+    variation2: ['Upper', 'Rest', 'Rest', 'Lower', 'Rest', 'Rest', 'Rest'],
+    variation3: ['Upper', 'Rest', 'Rest', 'Rest', 'Lower', 'Rest', 'Rest'],
+    variation4: ['Upper', 'Rest', 'Rest', 'Rest', 'Rest', 'Lower', 'Rest'],
+    variation5: ['Rest', 'Upper', 'Rest', 'Lower', 'Rest', 'Rest', 'Rest'],
+    variation6: ['Rest', 'Upper', 'Rest', 'Rest', 'Lower', 'Rest', 'Rest'],
+    variation7: ['Rest', 'Upper', 'Rest', 'Rest', 'Rest', 'Lower', 'Rest'],
+    variation8: ['Rest', 'Upper', 'Rest', 'Rest', 'Rest', 'Rest', 'Lower'],
+    variation9: ['Rest', 'Rest', 'Upper', 'Rest', 'Lower', 'Rest', 'Rest'],
+    variation10: ['Rest', 'Rest', 'Upper', 'Rest', 'Rest', 'Lower', 'Rest'],
+    variation11: ['Rest', 'Rest', 'Upper', 'Rest', 'Rest', 'Rest', 'Lower'],
+    variation12: ['Rest', 'Rest', 'Rest', 'Upper', 'Rest', 'Lower', 'Rest'],
+    variation13: ['Rest', 'Rest', 'Rest', 'Upper', 'Rest', 'Rest', 'Lower'],
+    variation14: ['Rest', 'Rest', 'Rest', 'Rest', 'Upper', 'Rest', 'Lower'],
+};
+
+export const threeDaySplits = {
+    variation1: ['Push', 'Rest', 'Pull', 'Rest', 'Legs', 'Rest', 'Rest'],
+    variation2: ['Rest', 'Push', 'Rest', 'Pull', 'Rest', 'Legs', 'Rest'],
+};
+
+export const fourDaySplits = {
+    variation1: ['UA', 'LA', 'Rest', 'UB', 'LB', 'Rest', 'Rest'],
+    variation2: ['UA', 'Rest', 'LA', 'UB', 'Rest', 'LB', 'Rest'],
+    variation3: ['UA', 'Rest', 'LA', 'Rest', 'UB', 'LB', 'Rest'],
+    variation4: ['UA', 'Rest', 'Rest', 'LA', 'UB', 'Rest', 'LB'],
+    variation5: ['Rest', 'UA', 'LA', 'Rest', 'UB', 'LB', 'Rest'],
+    variation6: ['Rest', 'UA', 'Rest', 'LA', 'UB', 'Rest', 'LB'],
+    variation7: ['Rest', 'Rest', 'UA', 'LA', 'Rest', 'UB', 'LB'],
+    variation8: ['Rest', 'Rest', 'UA', 'Rest', 'LA', 'UB', 'Rest'],
+    variation9: ['UB', 'LB', 'Rest', 'UA', 'LA', 'Rest', 'Rest'],
+    variation10: ['UB', 'Rest', 'LB', 'UA', 'Rest', 'LA', 'Rest'],
+    variation11: ['UB', 'Rest', 'LB', 'Rest', 'UA', 'LA', 'Rest'],
+    variation12: ['UB', 'Rest', 'Rest', 'LB', 'UA', 'Rest', 'LA'],
+    variation13: ['Rest', 'UB', 'LB', 'Rest', 'UA', 'LA', 'Rest'],
+    variation14: ['Rest', 'UB', 'Rest', 'LB', 'UA', 'Rest', 'LA'],
+    variation15: ['Rest', 'Rest', 'UB', 'LB', 'Rest', 'UA', 'Rest'],
+    variation16: ['Rest', 'Rest', 'UB', 'Rest', 'LB', 'UA', 'Rest'],
+};
+
+export const fiveDaySplits = {
+    variation1: ['Upper Body', 'Lower Body', 'Rest', 'Push', 'Pull', 'Legs', 'Rest'],
+    variation2: ['Upper Body', 'Rest', 'Lower Body', 'Push', 'Rest', 'Pull', 'Legs'],
+    variation3: ['Rest', 'Upper Body', 'Lower Body', 'Push', 'Rest', 'Pull', 'Legs'],
+    variation4: ['Upper Body', 'Lower Body', 'Push', 'Rest', 'Pull', 'Rest', 'Legs'],
+    variation5: ['Upper Body', 'Rest', 'Lower Body', 'Rest', 'Push', 'Pull', 'Legs'],
+    variation6: ['Rest', 'Upper Body', 'Pull', 'Rest', 'Lower Body', 'Push', 'Legs'],
+    variation7: ['Push', 'Rest', 'Pull', 'Legs', 'Rest', 'Upper Body', 'Lower Body'],
+    variation8: ['Pull', 'Rest', 'Legs', 'Push', 'Rest', 'Upper Body', 'Lower Body'],
+    variation9: ['Rest', 'Push', 'Pull', 'Rest', 'Upper Body', 'Lower Body', 'Legs'],
+    variation10: ['Push', 'Pull', 'Rest', 'Legs', 'Rest', 'Upper Body', 'Lower Body'],
+    variation11: ['Pull', 'Rest', 'Legs', 'Rest', 'Upper Body', 'Lower Body', 'Push'],
+    variation12: ['Rest', 'Pull', 'Legs', 'Rest', 'Push', 'Upper Body', 'Lower Body'],
+};
+
+export const sixDaySplits = {
+    // Rest at the Start
+    variation1: ['Rest', 'Push', 'Pull', 'Legs A', 'Push', 'Pull', 'Legs B'],
+    variation2: ['Rest', 'Push', 'Pull', 'Legs A', 'Pull', 'Push', 'Legs B'],
+    variation3: ['Rest', 'Push', 'Legs A', 'Pull', 'Push', 'Pull', 'Legs B'],
+    variation4: ['Rest', 'Pull', 'Push', 'Legs A', 'Push', 'Pull', 'Legs B'],
+    // Rest in the Middle
+    variation5: ['Push', 'Pull', 'Legs A', 'Rest', 'Push', 'Pull', 'Legs B'],
+    variation6: ['Push', 'Pull', 'Rest', 'Legs A', 'Push', 'Pull', 'Legs B'],
+    variation7: ['Push', 'Legs A', 'Pull', 'Rest', 'Push', 'Pull', 'Legs B'],
+    variation8: ['Pull', 'Push', 'Legs A', 'Rest', 'Pull', 'Push', 'Legs B'],
+    // Rest at the End
+    variation9: ['Push', 'Pull', 'Legs A', 'Push', 'Pull', 'Legs B', 'Rest'],
+    variation10: ['Push', 'Pull', 'Legs A', 'Pull', 'Push', 'Legs B', 'Rest'],
+    variation11: ['Push', 'Legs A', 'Pull', 'Push', 'Pull', 'Legs B', 'Rest'],
+    variation12: ['Pull', 'Push', 'Legs A', 'Push', 'Pull', 'Legs B', 'Rest'],
+};
