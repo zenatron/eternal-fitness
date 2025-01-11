55a587abe953737fe4de4c8a0024fe3016ed6545
Phil Vishnevsky <gadgetsphilop36@gmail.com>
2025-01-10 20:01:57 -0500

    add github pages

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client'; // Correct import for React 18+
+import ReactDOM from 'react-dom/client';
 import App from './components/App';
 import './styles/tailwind.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root')); // Create the root
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
