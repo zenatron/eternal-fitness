0f2810dfb4f1b7cdfc207b69221855235b599fdd
Phil Vishnevsky <gadgetsphilop36@gmail.com>
2025-01-13 08:59:32 -0500

    update packages

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/**/*.{js,jsx,ts,tsx}',
-    './public/index.html',
-  ],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'], // Adjust to match your project structure
   theme: {
-    extend: {},
+      extend: {},
   },
   plugins: [],
 };
